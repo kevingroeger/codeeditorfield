27b19e97a53dd5dae293f9f1e00482ffe34fedcd
Kevin Gröger <k.groeger@headtrip.eu>
2022-09-16 08:46:13 +0200

    reworking js

--- javascript/CodeEditorField.js
+++ javascript/CodeEditorField.js
@@ -1,36 +1,45 @@
-const textarea = $('textarea.codeeditor')
+(function($) {
+    $.entwine(function($) {
+        $("textarea.codeeditor").entwine({
+            onmatch: function() {
+                var textarea = this;
 
-// hide the textarea
-textarea.hide();
+                // hide the textarea
+                this.hide();
 
-// create the editor div
-const divID = textarea.attr('id')
-const $div = divID
+                // create the editor div
+                var divID = this.attr('id');
+                var $div = divID
 
-$($div).insertAfter(textarea);
-ace.config.set('modePath', textarea.data('ace-path'));
-ace.config.set('themePath', textarea.data('ace-path'));
-const editor = ace.edit(divID)
+                $($div).insertAfter(this);
+                ace.config.set('modePath', this.data('ace-path'));
+                ace.config.set('themePath', this.data('ace-path'));
+                var editor = ace.edit(divID);
 
-// make the editor update the textarea content
-editor.getSession().setValue(textarea.val());
-editor.getSession().on('change', function(){
-    textarea.val(editor.getSession().getValue());
-    textarea.change();
-});
+                // make the editor update the textarea content
+                editor.getSession().setValue(textarea.val());
+                editor.getSession().on('change', function(){
+                    textarea.val(editor.getSession().getValue());
+                    textarea.change();
+                });
 
-editor.setAutoScrollEditorIntoView(false);
-editor.getSession().setTabSize(2);
-editor.setShowPrintMargin(false);
-editor.session.setWrapLimitRange(null, null);
-editor.session.setUseWrapMode(true);
+                editor.setAutoScrollEditorIntoView(false);
+                editor.getSession().setTabSize(2);
+                editor.setShowPrintMargin(false);
+                editor.session.setWrapLimitRange(null, null);
+                editor.session.setUseWrapMode(true);
 
-// set the mode (ie syntax highlighting)
-editor.getSession().setMode(textarea.data('mode'));
-editor.setTheme('ace/theme/' + textarea.data('theme'));
-var lineHeight = (editor.renderer.lineHeight > 1 ? editor.renderer.lineHeight : 16)
+                // set the mode (ie syntax highlighting)
+                editor.getSession().setMode(this.data('mode'));
+                editor.setTheme('ace/theme/' + this.data('theme'));
+                var lineHeight = (editor.renderer.lineHeight > 1 ? editor.renderer.lineHeight : 16)
 
-$($div).css('min-height', lineHeight * textarea.attr('rows') + 35 + 'px');
+                $($div).css('min-height', lineHeight * textarea.attr('rows') + 35 + 'px');
 
-textarea.addClass('done');
+                this.addClass('done');
 
+                this._super();
+            },
+        });
+    });
+})(jQuery);
